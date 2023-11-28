639888bfd8c2e6320a74d8c5d110d6d1bb923496
Ilamparithi M <5023573ilamparithi@gmail.com>
2023-11-28 21:38:33 +0530

    oops forgot to cleanup

--- client_modules/encryption.py
+++ client_modules/encryption.py
@@ -2,13 +2,8 @@ from cryptography.hazmat.primitives import serialization, hashes, hmac
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives import padding as spadding
-from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-### For chat operations
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-# from cryptography.hazmat.primitives.asymmetric import ec
-# from cryptography.hazmat.primitives.kdf.hkdf import HKDF
-# from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from os import urandom
 from getpass import getpass
 from base64 import urlsafe_b64encode
@@ -99,37 +94,3 @@ def decrypt_data(encrypted_data, privkey):
         return pickle.loads(data)
     except Exception as error:
         return {'type': 'decrypt_error', 'data': f'{error}'}
-
-
-""" THIS PART OF THE MODULE IS RESERVED FOR CHAT OPERATIONS """
-
-# def create_key_pair():
-#    private_key_d = ec.generate_private_key(ec.SECP256K1())
-#    public_key_d = private_key_d.public_key()
-#    return private_key_d, public_key_d
-
-# def derive_key(eprkey, epbkey, keyinfo):
-#    shared_key = eprkey.exchange(
-#        ec.ECDH(), epbkey)
-#    # Perform key derivation.
-#    derived_key = HKDF(
-#        algorithm=hashes.SHA256(),
-#        length=32,
-#        salt=None,
-#        info=keyinfo.encode(),
-#    ).derive(shared_key)
-#    return derived_key
-
-# def encrypt_packet(data, key):
-#    data = pickle.dumps(data)
-#    aesgcm = AESGCM(key)
-#    nonce = urandom(12)  # Unique nonce for each message
-#    ciphertext = aesgcm.encrypt(nonce, data, None)
-#    return pickle.dumps({'nonce': nonce, 'ciphertext': ciphertext})
-
-# def decrypt_packet(data, key):
-#    aesgcm = AESGCM(key)
-#    nonce = data['nonce']
-#    ciphertext = data['ciphertext']
-#    plaintext = aesgcm.decrypt(nonce, ciphertext, None)
-#    return plaintext
